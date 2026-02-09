9507210e69977e85b4c839a3a231ee9fa0d4f12b
HoangPham <phamvanminhhoang3@gmail.com>
2026-02-10 00:29:39 +0700

    Add MongoDB connection setup and update dependencies

---
+++ src/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+        throw new Error('Missing MONGODB_URI in .env');
+    }
+
+    mongoose.connection.on('connected', () => {
+        console.log('✅ MongoDB connected');
+    })
+
+    mongoose.connection.on('error', (err) => {
+        console.error('❌ MongoDB connection error:', err);
+    })
+
+    mongoose.connection.on('disconnect', () => {
+        console.warn('⚠️ MongoDB disconnected');
+        
+    })
+
+    // Mongoose v6+ dùng option mặc định tốt, không cần useNewUrlParser...
+    await mongoose.connect(uri);
+
+}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+async function bootstrap(params) {
+
+  await connectDB();
+  
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
+
+bootstrap().catch((error) => {
+  console.error('❌ Failed to start server:', err);
+  process.exit(1);
+})
